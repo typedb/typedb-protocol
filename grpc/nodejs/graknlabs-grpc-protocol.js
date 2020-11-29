752402d73c94687888ae787991cfb74a76800505
Alistair Crook <alistair@grakn.ai>
2020-11-29 17:21:50 +0000

    Enable building npm package

---
+++ grpc/nodejs/graknlabs-grpc-protocol.js
@@ -0,0 +1,11 @@
+module.exports = {
+    answer_pb: require("protobuf/answer_pb"),
+    concept_pb: require("protobuf/concept_pb"),
+    database_pb: require("protobuf/database_pb"),
+    grakn_grpc_pb: require("protobuf/grakn_grpc_pb"),
+    grakn_pb: require("protobuf/grakn_pb"),
+    options_pb: require("protobuf/options_pb"),
+    query_pb: require("protobuf/query_pb"),
+    session_pb: require("protobuf/session_pb"),
+    transaction_pb: require("protobuf/transaction_pb")
+};
